7fb8ef7900ad78c35243f12f211cdea1e50e56d9
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-06-01 11:53:05 -0400

    Added "Spawn Swat" effect
    Spawns a S.W.A.T team (4 peds) to attack the player

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -372,6 +372,7 @@ namespace ConfigApp
             { "screen_colorfulworld", new EffectInfo("Colorful World", EffectCategory.Screen, true) },
             { "screen_arc", new EffectInfo("Arced Screen", EffectCategory.Screen, true, true) },
             { "world_blackhole", new EffectInfo("Black Hole", EffectCategory.Misc, true, true) },
+            { "spawn_swat", new EffectInfo("Spawn Swat Team", EffectCategory.Peds) },
         };
     }
 }

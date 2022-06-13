1cc23e81b26d235e2035fb994b024e4b5d60ad08
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-06-13 08:50:52 -0400

    Save Progress

--- ChaosMod/Effects/db/TestScript.cpp
+++ ChaosMod/Effects/db/TestScript.cpp
@@ -1,38 +1,67 @@
 #include <stdafx.h>
 
 #include "Memory/Vehicle.h"
+#include "Util/Vehicle.h"
 
-std::vector<Vehicle> vehiclesMap;
-
-static void OnStart()
+struct VehicleData
 {
+	float fRaise;
+	float fWheelWidth;
+	float fWheelSize;
+};
+
+std::map<Hash, VehicleData> vehiclesMap;
+
+static const float fWheelScale = 1.8f;
+
+static void OnStop()
+{
+	for (auto const &[model, data] : vehiclesMap)
+	{
+		LoadModel(model);
+		Vehicle temp = CREATE_VEHICLE(model, 0.f, 0.f, 0.f, 0.f, true, false, false);
+
+		Memory::SetVehicleRaise(temp, data.fRaise);
+		Memory::SetVehicleWheelSize(temp, data.fWheelSize);
+		Memory::SetVehicleWheelWidth(temp, data.fWheelWidth);
+
+		//Need to change the wheels again for the size to take effect
+		SET_VEHICLE_MOD(temp, 23, g_Random.GetRandomInt(0, 10), true);
+		SET_VEHICLE_WHEEL_TYPE(temp, g_Random.GetRandomInt(0, 7));
+
+		vehiclesMap.erase(model);
+	}
 	vehiclesMap.clear();
 }
 
 static void OnTick()
 {
-	Vehicle veh = GET_VEHICLE_PED_IS_IN(PLAYER_PED_ID(), false);
-	if (DOES_ENTITY_EXIST(veh))
+	for (Vehicle veh : GetAllVehs())
 	{
-		Memory::SetVehicleWheelSize(veh, 1.5f);
-		Memory::SetVehicleWheelWidth(veh, 1.5f);
-		Memory::SetVehicleSusHeight(veh, -1.5f);
-
-		if (std::find(vehiclesMap.begin(), vehiclesMap.end(), veh) == vehiclesMap.end())
+		if (!vehiclesMap.contains(GET_ENTITY_MODEL(veh)))
 		{
-			//Without custom wheels, we can't set the scale
-			SET_VEHICLE_MOD(veh, 23, g_Random.GetRandomInt(1, 50), true);
-			SET_VEHICLE_WHEEL_TYPE(veh, 1);
-			vehiclesMap.push_back(veh);
+			// Without custom wheels, we can't set the scale/width
+			SET_VEHICLE_MOD(veh, 23, g_Random.GetRandomInt(0, 10), true);
+			SET_VEHICLE_WHEEL_TYPE(veh, g_Random.GetRandomInt(0, 7));
+
+			float ogSize = Memory::GetVehicleWheelSize(veh), ogWidth = Memory::GetVehicleWheelWidth(veh);
+			Memory::SetVehicleRaise(veh, 1.f);
+
+			vehiclesMap.emplace(GET_ENTITY_MODEL(veh), VehicleData(0.f, ogWidth, ogSize));
 		}
+
+		Memory::SetVehicleTrackWidth(veh, 1.5f); // Has to be set every frame
+
+		Memory::SetVehicleWheelSize(veh, fWheelScale);
+		Memory::SetVehicleWheelWidth(veh, fWheelScale);
 	}
 }
 
 //	clang-format off
-REGISTER_EFFECT(OnStart, nullptr, OnTick, EffectInfo
+REGISTER_EFFECT(nullptr, OnStop, OnTick, EffectInfo
 	{ 
-		.Name = "_DBG",
-		.Id = "_DBG_",
+		.Name = "_MNSTR_TRUKS_",
+		.Id = "vehs_monster_trucks",
 		.IsTimed = true
 	}
 );
